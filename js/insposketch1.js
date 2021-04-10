29807c45b46b953070b1265205d23843260f7c08
stephaniehuang279 <80994889+stephaniehuang279@users.noreply.github.com>
2021-04-09 21:00:05 -0400

    updated test

---
+++ js/insposketch1.js
@@ -0,0 +1,11 @@
+//runs once and setups the scene
+function setup(){
+	createCanvas(600,600);
+}
+
+//runs FOREVER
+function draw(){
+	var rectWidth = 50;
+	var rectHeight = 100;
+	background("black");
+	
